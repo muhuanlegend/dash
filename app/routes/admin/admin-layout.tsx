4f4929bce6209ebf1dff92f113d5269503722bad
muhuanlegend <muhuanlegend@gmail.com>
2025-05-11 15:29:42 +0000

    Navigation added

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { MobileSidebar, NavItems } from "components";
+import { Outlet } from "react-router";
 
 const AdminLayout = () => {
   return (
-    <div className='admin-layout'>
-        MobileSidebar
-        <aside className='w-full max-w-[270px] hidden lg:block'>Sidebar</aside>
-        <aside className='children'>
-            <Outlet />
-        </aside>
-    </div>
-  )
-}
+    <div className="admin-layout">
+      <MobileSidebar />
 
-export default AdminLayout
+      <aside className="w-full max-w-[270px] hidden lg:block">
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
+      </aside>
+      <aside className="children">
+        <Outlet />
+      </aside>
+    </div>
+  );
+};
+
+export default AdminLayout;
