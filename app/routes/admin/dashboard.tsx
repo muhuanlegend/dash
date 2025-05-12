b58d9d85c74b7488a960c7629019b0244e29550b
muhuanlegend <muhuanlegend@gmail.com>
2025-05-12 16:14:20 +0000

    TripCard added

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,22 +1,11 @@
 import { Header, StatsCard, TripCard } from "components";
-import React from "react";
+import { allTrips, dashboardStats, user } from "~/constants";
+
+
+const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } =
+  dashboardStats;
 
 const Dashboard = () => {
-  const user = {
-    name: "Miles",
-  };
-
-  const dashboardStats = {
-    totalUsers: 12450,
-    usersJoined: { currentMonth: 218, lastMonth: 176 },
-    totalTrips: 3120,
-    tripsCreated: { currentMonth: 150, lastMonth: 250 },
-    userRole: { total: 62, currentMonth: 25, lastMonth: 15 },
-  };
-
-  const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } =
-    dashboardStats;
-
   return (
     <main className="dashboard wrapper">
       <Header
@@ -46,7 +35,21 @@ const Dashboard = () => {
         </div>
       </section>
 
-      <TripCard />
+      <section className="container">
+        <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
+        <div className="trip-grid">
+          {allTrips.slice(0, 4).map(({id, name, imageUrls, itinerary, tags, estimatedPrice}) => (
+            <TripCard key={id} 
+            id={id.toString()}
+            name={name}
+            imageUrl={imageUrls[0]}
+            location={itinerary[0]?.location ?? ''}
+            tags={tags}
+            price={estimatedPrice}
+            />
+          ))}
+        </div>
+      </section>
     </main>
   );
 };
